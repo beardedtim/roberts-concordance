32029b32fd215d7eaa85297f3f897a4611d25df7
Tim Roberts <timiroberts@gmail.com>
2023-01-16 11:22:48 -0500

    Basic Index Working
    We are able to ask the system for verses that match full strings.
    There is no stemming yet

--- cmd/parse/main.go
+++ cmd/parse/main.go
@@ -4,6 +4,9 @@
 
 	Parses artifacts/books.txt into a hash of Books: [Verses]
 	and outputs that hash into artifacts/parsed/bible.json
+
+	Parses artifacts/raw.json into an index of where each word
+	is used throughout the Bible and outputs into artifacts/parsed/index.json
 */
 
 package main
@@ -37,6 +40,14 @@ type BibleBook struct {
 	Book   string       `json:"book"`
 	Verses []BibleVerse `json:"verses"`
 }
+
+type WordIndex struct {
+	Book    string `json:"book"`
+	Chapter int    `json:"chapter"`
+	Verse   int    `json:"verse"`
+}
+
+type Index map[string][]WordIndex
 
 func (f *Field) UnmarshalJSON(p []byte) error {
 	var tmp []interface{}
@@ -141,6 +152,8 @@ func main() {
 		books[i] = book
 	}
 
+	wordIndex := make(Index)
+
 	for _, field := range verses {
 		verse := field
 
@@ -148,19 +161,57 @@ func main() {
 
 		book := books[bookIndex]
 
-		book.Verses = append(book.Verses, BibleVerse{
+		// Make book
+		verseHash := BibleVerse{
 			Book:    book.Book,
 			Chapter: verse.Chapter,
 			Verse:   verse.Verse,
 			Text:    verse.Text,
 			Notes:   verse.Notes,
-		})
+		}
+		book.Verses = append(book.Verses, verseHash)
 
 		books[bookIndex] = book
+
+		// make index
+		text := verseHash.Text
+		var nonAlphanumericRegex = regexp.MustCompile(`[^a-zA-Z0-9\-]+`)
+
+		for _, word := range strings.Fields(text) {
+			index := nonAlphanumericRegex.ReplaceAllString(word, "")
+			if strings.Contains(index, "-") {
+				// if the word is hyphenated, let's make sure that we
+				// index all three options. Ex:
+				//
+				// wood-offering becomes
+				// wood
+				// offering
+				// wood-offering
+				split := strings.Split(index, "-")
+
+				for _, splitWord := range split {
+					wordIndex[splitWord] = append(wordIndex[splitWord], WordIndex{
+						Book:    verseHash.Book,
+						Chapter: verse.Chapter,
+						Verse:   verse.Verse,
+					})
+				}
+			}
+
+			wordIndex[index] = append(wordIndex[word], WordIndex{
+				Book:    verseHash.Book,
+				Chapter: verse.Chapter,
+				Verse:   verse.Verse,
+			})
+		}
 	}
 
 	bibleBook, _ := json.MarshalIndent(books, "", " ")
 
 	// Write Verses File
 	_ = os.WriteFile(globals.ArtifactsDir()+"/parsed/bible.json", bibleBook, 0644)
+	index, _ := json.MarshalIndent(wordIndex, "", " ")
+
+	// Write Index File
+	_ = os.WriteFile(globals.ArtifactsDir()+"/parsed/index.json", index, 0644)
 }
